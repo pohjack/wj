bc287166beccd35fab23022fb94710f1da00d481
伍舰 <wujian3@asiainfo.com>
2018-08-23 14:10:13 +0800

    更新

--- src/main/java/com/java8newfeatures/bean/Person.java
+++ src/main/java/com/java8newfeatures/bean/Person.java
@@ -1,9 +1,11 @@
 package com.java8newfeatures.bean;
 
+import java.util.Objects;
+
 public class Person {
     public String name;
     public int age;
-    String address;
+    public String address;
     public Person(String name,int age,String address){
         this.address=address;
         this.age=age;
@@ -13,5 +15,24 @@ public class Person {
 	public String toString() {
 		return "Person [name=" + name + ", age=" + age + ", address=" + address + "]";
 	}
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, age, address);
+    }
 }
